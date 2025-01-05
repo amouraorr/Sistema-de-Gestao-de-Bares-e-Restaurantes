b50c53681e4537605ae156e3a486ed3194649f86
amouraorr <amouraorr@yahoo.com.br>
2025-01-05 14:40:09 -0300

    Criação da Entities e ajustes

---
+++ src/main/java/com/webdevelopment/entities/User.java
@@ -0,0 +1,35 @@
+package com.webdevelopment.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String usename;
+
+    private String password;
+
+    private String email;
+
+    private String role;
+
+    @Column(name = "data_registration")
+    private LocalDateTime dataRegistration;
+
+    @Column(name = "last_login")
+    private String last_login;
+
+    private Boolean status;
+
+}
