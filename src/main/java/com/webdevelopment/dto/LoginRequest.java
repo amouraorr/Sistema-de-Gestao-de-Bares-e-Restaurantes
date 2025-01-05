2e61e0f433f2ecca193c67bf152c034bfed21c12
amouraorr <amouraorr@yahoo.com.br>
2025-01-05 16:21:35 -0300

    Criação do AuthService e ajustes

--- src/main/java/com/webdevelopment/dto/LoginRequest.java
+++ src/main/java/com/webdevelopment/dto/LoginRequest.java
@@ -1,4 +1,12 @@
 package com.webdevelopment.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+
+    private String username;
+    private String password;
 }
---
+++ src/main/java/com/webdevelopment/service/AuthService.java
@@ -0,0 +1,35 @@
+package com.webdevelopment.service;
+
+import com.webdevelopment.dto.LoginRequest;
+import com.webdevelopment.dto.LoginResponse;
+import com.webdevelopment.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        Optional<User> userOptional = userService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+
+            String token = jwtService.genarationToken(user.getUsername());
+            return new LoginResponse(token, user.getUsername(), user.getUsername(), user.getEmail());
+        } else {
+            return null;
+        }
+
+    }
+
+}
