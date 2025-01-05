43ee634b7cef4a841f0434b889d0551d10bd97c4
amouraorr <amouraorr@yahoo.com.br>
2025-01-05 17:58:04 -0300

    Criação do TableRepository e ajustes

---
+++ src/main/java/com/webdevelopment/repositories/TableRepository.java
@@ -0,0 +1,8 @@
+package com.webdevelopment.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableRepository extends JpaRepository<TableEntity, Long> {
+    
+}
