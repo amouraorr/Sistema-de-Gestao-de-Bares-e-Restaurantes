e55157f8fcb6201be843407464429442bfef6bd6
amouraorr <amouraorr@yahoo.com.br>
2025-01-05 18:00:49 -0300

    Criação do Credentials e ajustes

---
+++ src/main/java/com/webdevelopment/entities/Credentials.java
@@ -0,0 +1,11 @@
+package com.webdevelopment.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Credentials {
+    private String username;
+    private String password;
+}
